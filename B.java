7d73cacb21117ead03be87b5ebc5492d58ed779e
Gayatri <gayatrilonkar048@gmail.com>
2025-08-11 11:26:44 +0530

    B.java added

---
+++ B.java
@@ -0,0 +1,31 @@
+public class B {
+public static void main m1 (){
+
+int a = 10 ;
+int b= 20 ;
+int c = a+b ;
+System.out.println(c)
+
+
+
+}
+
+
+public static void main (String args[]){
+
+
+B.m1();
+
+
+
+
+
+
+}
+
+
+
+
+
+
+}
